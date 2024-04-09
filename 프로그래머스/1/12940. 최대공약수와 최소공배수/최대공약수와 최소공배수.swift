838198bb9e7e829666830f3830d834645215014e
jimmy <71679088+wjdtjq6@users.noreply.github.com>
2024-04-09 13:56:47 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.03 ms, Memory: 16.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.swift
@@ -0,0 +1,13 @@
+ 
+ 	
+func solution(_ n:Int, _ m:Int) -> [Int] {
+    var gcd = 0
+    
+    for i in 1...min(n, m) {
+        if n % i == 0 && m % i == 0 {
+            gcd = i
+        }
+    }
+    
+    return [gcd, (n * m) / gcd]
+}
