ee811fcb6792ec1b70fdeaf5e89a30c52b13df95
jimmy <71679088+wjdtjq6@users.noreply.github.com>
2024-04-11 14:25:54 +0900

    [level 1] Title: 크기가 작은 부분문자열, Time: 0.07 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.swift
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.swift
@@ -1,18 +1,14 @@
 import Foundation
 
 func solution(_ t: String, _ p: String) -> Int {
-    var index1 = 0
-    let index2 = p.count
-    var answer = 0
-    for i in 0..<t.count where index1 + index2 <= t.count {
-        let start = t.index(t.startIndex, offsetBy: index1)
-        let end = t.index(t.startIndex, offsetBy: index1 + index2)
-        let result = String(t[start..<end])
-        index1 += 1
-        
-        if result <= p {
-            answer += 1
+    let pValue = Int(p)!
+    var count = 0
+    for i in 0...(t.count - p.count) {
+        let subString = String(t[t.index(t.startIndex, offsetBy: i)..<t.index(t.startIndex, offsetBy: i + p.count)])
+        let subValue = Int(subString)!
+        if subValue <= pValue {
+            count += 1
         }
     }
-    return answer
+    return count
 }
