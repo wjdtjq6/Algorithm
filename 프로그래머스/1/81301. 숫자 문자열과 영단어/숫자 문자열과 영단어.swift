dae88815c8262af126960b6100918b73b28095c1
jimmy <71679088+wjdtjq6@users.noreply.github.com>
2024-04-14 03:22:43 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.14 ms, Memory: 16.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+func solution(_ s:String) -> Int {
+    let numArray = ["zero","one","two","three","four","five","six","seven","eight","nine"]
+    var resultArray = s
+    for i in 0..<numArray.count {
+        resultArray = resultArray.replacingOccurrences(of:numArray[i],with:"\(i)")
+    }
+    return Int(resultArray)!
+}
+ 
