4f94445f259ffd9f8a61e64a38b6ca4c0735b285
jimmy <71679088+wjdtjq6@users.noreply.github.com>
2024-04-24 00:21:27 +0900

    [level 1] Title: 콜라 문제, Time: 7.94 ms, Memory: 16.1 MB -BaekjoonHub

--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.swift
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.swift
@@ -2,12 +2,20 @@ import Foundation
 
 func solution(_ a:Int, _ b:Int, _ n:Int) -> Int {
     var n = n
+    var a = a
+    var b = b
     var result = 0
     for i in 0...n where  n > b {
-        let numbers = n / a
-        n = n - numbers * a
-        n = n + numbers * b
-        result += numbers * b
+        n /= a
+        // n *= b
+        // result += n
+        n = n - (n * a)
+        n = n + (n * b)
+        result += n * b
     }
+    let height = 160
+    let weight = 50
+    let waterGuide: Double = (height + weight) / Double(100)
+    print("\(height) \(weight) \(waterGuide)")
     return result
 }
